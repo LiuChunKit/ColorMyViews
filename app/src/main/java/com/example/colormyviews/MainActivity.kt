4bf8c602583e27a7c570535cec46eb236ac7ae41
Chunkit2796@gmail.com <Chunkit0721>
2019-10-31 11:00:40 +0800

    ColorMyViews

---
+++ app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.colormyviews
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //create btn and listener
+
+        //val clickableViews: List<View> = listOf(8 items)
+
+        //for(item in clickableViews){
+        //    item.setOnClickListener{makeColored(it)}
+        //}
+    }
+
+    //passing in textView & button
+
+
+}
